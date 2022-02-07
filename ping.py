9dbcf6ecb9f1382347c0d344fb55a2f0cf10a5d3
mohammadshoyab <91404016+mohammadshoyab@users.noreply.github.com>
2022-02-07 12:05:40 +0530

    Update ping.py
    i'm not any professional coder just learning the things 
    just started with python ,  no codin backgroud

--- ping.py
+++ ping.py
@@ -1,4 +1,4 @@
-#creating aping pong game
+#creating a ping pong game
 from time import sleep
 import turtle
 import random
@@ -20,46 +20,59 @@ for i in range(4):
     bon.left(90)
 bon.hideturtle()
 
+# to create block 1( or player 1)
 s1=turtle.Turtle()
 s1.shape('square')
 s1.shapesize(stretch_wid=5,stretch_len=2)
 s1.penup()
 s1.goto(280,0)
 
-
+#creating player 2
 s2=turtle.Turtle()
 s2.shape('square')
 s2.shapesize(stretch_wid=5,stretch_len=2)
 s2.penup()
 s2.goto(-280,0)
 
+# creating a movable object 
 player=turtle.Turtle()
-player.color("black")
-player.shape("circle")
+player.color("black") # any colour u wish
+player.shape("circle") # any shape you can 
 player.penup()
-player.setpos(0,0)
+player.setpos(0,0) # you can go to anypos
 player.speed(0)
 
+
+# creating functions 
+
+# for left block moving up
 def leftu():
     y=s2.ycor()
     y+=20
     s2.sety(y)
+
+# for left block moving down
 
 def leftd():
     y=s2.ycor()
     y -= 20
     s2.sety(y)
 
+    
+# for right block moving up
 def rightu():
     y=s1.ycor()
     y+=20
     s1.sety(y)
 
+    
+# for right block moving down
 def rightd():
     y=s1.ycor()
     y -= 20
     s1.sety(y)
 
+# this to check the collision between borders 
 def iscolli(t1,t2):
     d=math.sqrt(math.pow(t1.xcor()-t2.xcor(),2) + math.pow(t1.ycor()-t2.ycor(),2))
     if d < 35 :
@@ -67,9 +80,15 @@ def iscolli(t1,t2):
     else:
         return False
 
+    
+    #assigning keys  you can have any keys from a-z or 0-9 or arrows
+    
 sc.listen()
-sc.onkeypress(leftu,"w")
+#left player keys
+sc.onkeypress(leftu,"w") 
 sc.onkeypress(leftd,"x")
+
+#right player keys
 sc.onkeypress(rightu,"Up")
 sc.onkeypress(rightd,"Down")
 
@@ -100,7 +119,7 @@ while True:
         player.left(random.choice(angles))
         p2 += 1
 
-
+# score turtle
     bon.undo()
     bon.penup()
     bon.hideturtle()
